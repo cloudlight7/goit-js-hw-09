2980c03f0566495c6c14e52d701106329468112a
cloudlight7 <49961563+cloudlight7@users.noreply.github.com>
2022-08-20 18:17:46 +0000

    Deploying to gh-pages from @ cloudlight7/goit-js-hw-09@474324fbc7389aa8d8500e056979d6a5d6c35547 🚀

---
+++ 01-color-switcher.cd3beeec.js
@@ -0,0 +1,2 @@
+!function(){var e,t=document.querySelector("[data-start]"),d=document.querySelector("[data-stop]"),a=document.querySelector("body");d.disabled=!0,t.addEventListener("click",(function(){e=setInterval((function(){a.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.disabled=!0,d.disabled=!1})),d.addEventListener("click",(function(){clearInterval(e),t.disabled=!1,d.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.cd3beeec.js.map
